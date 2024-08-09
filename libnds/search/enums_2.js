4af6b790ec4021a0e71a91da23e749fbac9efe10
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-08-09 01:46:16 +0100

    Fri  9 Aug 01:46:16 BST 2024

--- libnds/search/enums_2.js
+++ libnds/search/enums_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['cdcbanks_0',['cdcBanks',['../codec_8h.html#aa2a62d43c632603b2fec1f951d003a55',1,'codec.h']]],
-  ['clockdivider_1',['ClockDivider',['../timers_8h.html#a2885908e39e7576d7277f9d97c1c14f7',1,'timers.h']]],
-  ['consolecolor_2',['ConsoleColor',['../console_8h.html#a821042d5f30537f776ce823c5d66aada',1,'console.h']]]
+  ['cameradevice_0',['CameraDevice',['../arm9_2camera_8h.html#a277786e6d2dbc6a3a5f887d60a9c9f74',1,'camera.h']]],
+  ['cdcbanks_1',['cdcBanks',['../codec_8h.html#aa2a62d43c632603b2fec1f951d003a55',1,'codec.h']]],
+  ['clockdivider_2',['ClockDivider',['../timers_8h.html#a2885908e39e7576d7277f9d97c1c14f7',1,'timers.h']]],
+  ['consolecolor_3',['ConsoleColor',['../console_8h.html#a821042d5f30537f776ce823c5d66aada',1,'console.h']]]
 ];
--- libnds/search/files_0.js
+++ libnds/search/files_0.js
@@ -1,9 +1,11 @@
 var searchData=
 [
-  ['arm7_2finput_2eh_0',['input.h',['../arm7_2input_8h.html',1,'']]],
-  ['arm7_2ftouch_2eh_1',['touch.h',['../arm7_2touch_8h.html',1,'']]],
-  ['arm9_2fcard_2eh_2',['card.h',['../arm9_2card_8h.html',1,'']]],
-  ['arm9_2finput_2eh_3',['input.h',['../arm9_2input_8h.html',1,'']]],
-  ['arm9_2fsdmmc_2eh_4',['sdmmc.h',['../arm9_2sdmmc_8h.html',1,'']]],
-  ['audio_2eh_5',['audio.h',['../audio_8h.html',1,'']]]
+  ['arm7_2fcamera_2eh_0',['camera.h',['../arm7_2camera_8h.html',1,'']]],
+  ['arm7_2finput_2eh_1',['input.h',['../arm7_2input_8h.html',1,'']]],
+  ['arm7_2ftouch_2eh_2',['touch.h',['../arm7_2touch_8h.html',1,'']]],
+  ['arm9_2fcamera_2eh_3',['camera.h',['../arm9_2camera_8h.html',1,'']]],
+  ['arm9_2fcard_2eh_4',['card.h',['../arm9_2card_8h.html',1,'']]],
+  ['arm9_2finput_2eh_5',['input.h',['../arm9_2input_8h.html',1,'']]],
+  ['arm9_2fsdmmc_2eh_6',['sdmmc.h',['../arm9_2sdmmc_8h.html',1,'']]],
+  ['audio_2eh_7',['audio.h',['../audio_8h.html',1,'']]]
 ];
